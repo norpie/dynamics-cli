2ad5dc47188e6f90f8f4c72d46a12854cf5fed02
norpie (Konsta Kuosmanen) <contact@norpie.dev>
2025-09-28 16:10:02 +0200

    chore: delete old auth

--- src/commands/auth/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod remove;
-pub mod select;
-pub mod setup;
-pub mod status;
-
-pub use remove::remove_command;
-pub use select::select_command;
-pub use setup::{SetupOptions, setup_command};
-pub use status::status_command;
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,4 +1,3 @@
-pub mod auth;
 pub mod deadlines;
 pub mod entity;
 pub mod migration;
